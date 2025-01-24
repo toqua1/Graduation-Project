e9471c161e8b7ab120a1dd6d7b31cf8a9ee85ed5
ToquaMagdy <toquamagdy25@gmail.com>
2025-01-24 18:43:24 +0200

    set up exercise page

--- lib/features/Workouts/presentation/view/exercise_screen.dart
+++ lib/features/Workouts/presentation/view/exercise_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
-import 'package:graduation_project/core/constants.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:graduation_project/features/widgets/custom_app_bar.dart';
 
 class ExerciseScreen extends StatelessWidget {
   const ExerciseScreen({super.key});
@@ -12,52 +11,9 @@ class ExerciseScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              // Back Button
-              NeumorphicButton(
-                style: NeumorphicStyle(
-                  depth: -4,
-                  intensity: 0.8,
-                  shape: NeumorphicShape.flat,
-                  color: Colors.white,
-                  boxShape: NeumorphicBoxShape.roundRect(
-                    BorderRadius.circular(8),
-                  ),
-                ),
-                padding: const EdgeInsets.all(10.0), // Button padding
-                child: const Icon(Icons.arrow_back_ios_new_rounded,
-                    color: AppColors.primaryColor),
-                onPressed: () {
-                  // Action for back button
-                },
-              ),
-
-              // Title Text
-              Text('Arm Exercise', style: AppFonts.headline1),
-
-              // More Options Button
-              NeumorphicButton(
-                style: NeumorphicStyle(
-                  depth: -4,
-                  intensity: 0.8,
-                  shape: NeumorphicShape.flat,
-                  color: Colors.white,
-                  boxShape: NeumorphicBoxShape.roundRect(
-                    BorderRadius.circular(8),
-                  ),
-                ),
-                padding: const EdgeInsets.all(15), // Button padding
-                child: SvgPicture.asset(
-                  'lib/assets/Vector.svg',
-                  height: 12,
-                  width: 12,
-                ),
-                onPressed: () {
-                  // Action for back button
-                },
-              ),
+              CustomAppBar(),
             ],
           ),
         ),
